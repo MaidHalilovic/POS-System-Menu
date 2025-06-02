1187ad884efc773191f0a8230717fcbbb46a0daf
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-06-02 23:39:07 +0200

    Testing api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Routes />
+          <Route path='/' element={<HomePage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [menu, setMenu] = useState(null);
+
+  const fetchMenu = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://connect.squareupsandbox.com/v2/catalog/list`
+      );
+
+      console.log(data);
+      setMenu(data);
+    } catch (error) {
+      console.error("Error while fetching products", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
   return <div>HomePage</div>;
 };
 
