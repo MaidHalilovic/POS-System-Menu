3d41e4ea45be7cb4343f966d3c8435aa78ab104f
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-06-03 23:37:54 +0200

    Made Header expect input

---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./header.css";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Autocomplete from "@mui/material/Autocomplete";
+
+const Header = () => {
+  return (
+    <div className='mainDiv'>
+      <div className='pos'>
+        <h1>
+          <span style={{ color: "#7AC6D2" }}>P</span>
+          <span style={{ color: "#E78B48" }}>O</span>
+          <span style={{ color: "#67AE6E" }}>S</span>
+        </h1>
+      </div>
+      <div className='search'>
+        <Stack spacing={2} sx={{ width: 300 }}>
+          <Autocomplete
+            id='free-solo-demo'
+            freeSolo
+            // options={top100Films.map((option) => option.title)}
+            renderInput={(params) => (
+              <TextField {...params} label='Search product or any order...' />
+            )}
+          />
+          {/* input is not working for now */}
+        </Stack>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
