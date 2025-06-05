686bf78dac73e3253772e0c31ffa4fb1b6a7ad09
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-06-06 01:25:14 +0200

    Making cards

---
+++ src/components/ContainerHomePage/ContainerHomePage.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./containerHomePage.css";
+
+const ContainerHomePage = () => {
+  const [menu, setMenu] = useState([]);
+
+  const fetchMenu = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://free-food-menus-api-two.vercel.app/burgers `
+      );
+
+      console.log(data);
+      setMenu(data);
+    } catch (error) {
+      console.error("Error while fetching products", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+  return (
+    <div>
+      <div className='menu'>
+        <h2>Special Menu For You</h2>
+      </div>
+
+      {menu && menu.length > 0 ? (
+        <>
+          <div className='meals'>
+            <img
+              src={menu[0].img}
+              alt='img'
+              style={{ width: 100, height: 100 }}
+            />
+            <h3>{menu[0].name}</h3>
+            <div className='price'>
+              <p>Price:</p>
+              <p>${menu[1].price}</p>
+            </div>
+          </div>
+          <div className='meals'>
+            <img
+              src={menu[1].img}
+              alt='img'
+              style={{ width: 100, height: 100 }}
+            />
+            <h3>{menu[1].name}</h3>
+            <div className='price'>
+              <p>Price:</p>
+              <p>${menu[1].price}</p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>Učitavanje...</p>
+      )}
+    </div>
+  );
+};
+
+export default ContainerHomePage;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,39 +1,11 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./homePage.css";
+import ContainerHomePage from "../../components/ContainerHomePage/ContainerHomePage";
 
 const HomePage = () => {
-  const [menu, setMenu] = useState(null);
-
-  const fetchMenu = async () => {
-    try {
-      const { data } = await axios.get(
-        `https://free-food-menus-api-two.vercel.app/burgers `
-      );
-
-      console.log(data);
-      setMenu(data);
-    } catch (error) {
-      console.error("Error while fetching products", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
   return (
     <div className='homePage'>
-      <div className='menu'>
-        <h2>Special Menu For You</h2>
-      </div>
-      <div className='cards'>
-        {menu.map((index, el) => (
-          <div className='card' key={index}>
-            <h1>{el[0].id}</h1>
-          </div>
-        ))}
-      </div>
+      <ContainerHomePage />
     </div>
   );
 };
