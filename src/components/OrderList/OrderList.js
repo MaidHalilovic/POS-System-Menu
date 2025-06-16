0333c70a6cee447e6b1e6d711ab20b1a9009235f
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-06-16 23:30:02 +0200

    Making orderList

--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./orderList.css";
+import axios from "axios";
 
 const OrderList = () => {
+  const [menu, setMenu] = useState([]);
+
+  const fetchMenu = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://free-food-menus-api-two.vercel.app/burgers `
+      );
+      setMenu(data);
+    } catch (error) {
+      console.error("Error while fetching products", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
   return (
     <div className='orderList'>
-      <p>Order</p>
+      <div className='orderList-header'>
+        <span>Table 01</span>
+      </div>
+      <div className='orderItem'></div>
     </div>
   );
 };
