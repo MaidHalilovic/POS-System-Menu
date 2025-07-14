21563942868084a3afb259d9268497bea1ed14e6
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-07-14 16:44:20 +0200

    Add a photo while clicking add product

--- src/OrderContext/OrderContext.js
+++ src/OrderContext/OrderContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState } from "react";
+import "./orderList.css";
 
 const OrderContext = createContext();
 
@@ -26,3 +27,43 @@ export const OrderProvider = ({ children }) => {
     </OrderContext.Provider>
   );
 };
+
+const OrderList = () => {
+  const { order } = useOrder();
+
+  return (
+    <div className='orderList'>
+      <div className='orderList-header'>
+        <span>Table</span>
+      </div>
+      <div className='orderItem'>
+        {order.length === 0 ? (
+          <p>No products added.</p>
+        ) : (
+          order.map((item) => (
+            <div
+              key={item.id}
+              className='order-row'
+              style={{ display: "flex", alignItems: "center", gap: "10px" }}
+            >
+              <img
+                src={item.img}
+                alt={item.name}
+                style={{
+                  width: "40px",
+                  height: "40px",
+                  borderRadius: "8px",
+                  objectFit: "cover",
+                }}
+              />
+              <span>{item.name}</span>
+              <span>x{item.quantity}</span>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default OrderList;
